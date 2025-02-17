4f6f15a107d113e15574085244f8df4f7881a822
sjoleee <cent7425@gmail.com>
2025-02-17 21:26:13 +0900

    첫번째 테스트 코드 작성

---
+++ playground/src/이상조/utils/password-verifier0.test.ts
@@ -0,0 +1,12 @@
+import verifyPassword from "./password-verifier0";
+
+test("잘못된 이름이 붙은 테스트", () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const 잘못된_룰 = (_: string) => {
+    return { passed: false, reason: "그냥 실패함" };
+  };
+
+  const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+
+  expect(errors[0]).toMatch("그냥 실패함");
+});
