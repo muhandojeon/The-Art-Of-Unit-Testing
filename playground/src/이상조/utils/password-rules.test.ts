43c6b2bdef5d0a14f4569db101698f4608bdae0b
sjoleee <cent7425@gmail.com>
2025-02-18 22:20:02 +0900

    코드 수정, each 사용

--- playground/src/이상조/utils/password-rules.test.ts
+++ playground/src/이상조/utils/password-rules.test.ts
@@ -2,21 +2,34 @@ import oneUpperCaseRule from "./password-rules";
 
 describe("oneUpperCaseRule", () => {
   test("대문자가 없는 경우 실패한다.", () => {
-    expect(oneUpperCaseRule("password")).toBe(false);
+    const result = oneUpperCaseRule("password");
+    expect(result.passed).toEqual(false);
   });
 
   test("대문자가 있는 경우 성공한다.", () => {
-    expect(oneUpperCaseRule("Password")).toBe(true);
+    const result = oneUpperCaseRule("Password");
+    expect(result.passed).toEqual(true);
   });
 
   test("또다른 대문자가 있는 경우 성공한다.", () => {
-    expect(oneUpperCaseRule("PASSWORD")).toBe(true);
+    const result = oneUpperCaseRule("PASSWORD");
+    expect(result.passed).toEqual(true);
   });
 
   test.each(["Password", "PASSWORD"])(
     "대문자가 있는 경우 성공한다.",
     (password) => {
-      expect(oneUpperCaseRule(password)).toBe(true);
+      const result = oneUpperCaseRule(password);
+      expect(result.passed).toEqual(true);
     }
   );
+
+  test.each([
+    ["Password", true],
+    ["PASSWORD", true],
+    ["password", false],
+  ])("%s 가 입력되면 %s 반환한다.", (password, expected) => {
+    const result = oneUpperCaseRule(password);
+    expect(result.passed).toEqual(expected);
+  });
 });
--- playground/src/이상조/utils/password-rules.ts
+++ playground/src/이상조/utils/password-rules.ts
@@ -1,6 +1,6 @@
 const oneUpperCaseRule = (input: string) => {
   return {
-    passed: input.toUpperCase() !== input,
+    passed: input.toLowerCase() !== input,
     reason: "대문자가 하나 이상 포함되어야 합니다.",
   };
 };
