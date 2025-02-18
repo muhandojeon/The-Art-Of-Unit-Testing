db9a2d9226253fb8827a4bc52762aa44cfa06878
sjoleee <cent7425@gmail.com>
2025-02-18 21:26:12 +0900

    it 함수 사용

--- playground/src/이상조/utils/password-verifier0.test.ts
+++ playground/src/이상조/utils/password-verifier0.test.ts
@@ -64,3 +64,18 @@ describe("verifyPassword", () => {
     });
   });
 });
+
+describe("verifyPassword", () => {
+  describe("잘못된 룰을 입력할 경우", () => {
+    it("에러를 반환한다.", () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const 잘못된_룰 = (_: string) => {
+        return { passed: false, reason: "그냥 실패함" };
+      };
+
+      const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+
+      expect(errors[0]).toContain("그냥 실패함");
+    });
+  });
+});
