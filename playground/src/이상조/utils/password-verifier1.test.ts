9a9c1b71e1b16519fc890d9e0ce21d381cadc98f
sjoleee <cent7425@gmail.com>
2025-02-18 21:53:52 +0900

    describe 제거

--- playground/src/이상조/utils/password-verifier1.test.ts
+++ playground/src/이상조/utils/password-verifier1.test.ts
@@ -185,14 +185,48 @@ describe("PasswordVerifier1", () => {
   describe("성공하는 룰과 실패하는 룰을 입력할 경우", () => {
     it("에러는 하나만 존재한다.", () => {
       const verifier = makeVerifierWith성공Rules();
+      verifier.addRule(make_실패하는_룰("그냥 실패함"));
       const errors = verifier.verify("아무 값이나 넣음");
       expect(errors.length).toBe(1);
     });
 
     it("에러는 실패하는 룰의 에러를 반환한다.", () => {
       const verifier = makeVerifierWith성공Rules();
+      verifier.addRule(make_실패하는_룰("그냥 실패함"));
       const errors = verifier.verify("아무 값이나 넣음");
       expect(errors[0]).toContain("그냥 실패함");
     });
   });
+});
+
+test("verifier에 실패하는 룰을 입력할 경우 rule.reason에 기반한 에러를 반환한다.", () => {
+  const verifier = makeVerifierWith실패Rules();
+  const errors = verifier.verify("아무 값이나 넣음");
+  expect(errors[0]).toContain("그냥 실패함");
+});
+
+test("verifier에 실패하는 룰을 입력할 경우 에러는 하나만 존재한다.", () => {
+  const verifier = makeVerifierWith실패Rules();
+  const errors = verifier.verify("아무 값이나 넣음");
+  expect(errors.length).toBe(1);
+});
+
+test("verifier에 성공하는 룰을 입력할 경우 에러는 하나도 존재하지 않는다.", () => {
+  const verifier = makeVerifierWith성공Rules();
+  const errors = verifier.verify("아무 값이나 넣음");
+  expect(errors.length).toBe(0);
+});
+
+test("verifier에 성공하는 룰과 실패하는 룰을 입력할 경우 에러는 하나만 존재한다.", () => {
+  const verifier = makeVerifierWith성공Rules();
+  verifier.addRule(make_실패하는_룰("그냥 실패함"));
+  const errors = verifier.verify("아무 값이나 넣음");
+  expect(errors.length).toBe(1);
+});
+
+test("verifier에 성공하는 룰과 실패하는 룰을 입력할 경우 에러는 실패하는 룰의 에러를 반환한다.", () => {
+  const verifier = makeVerifierWith성공Rules();
+  verifier.addRule(make_실패하는_룰("그냥 실패함"));
+  const errors = verifier.verify("아무 값이나 넣음");
+  expect(errors[0]).toContain("그냥 실패함");
 });
