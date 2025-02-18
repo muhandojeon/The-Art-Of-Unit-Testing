27f105f380eb8c39a37ec298b8084e478b63fcfd
sjoleee <cent7425@gmail.com>
2025-02-18 21:31:49 +0900

    번역 수정

--- playground/src/이상조/utils/password-verifier0.test.ts
+++ playground/src/이상조/utils/password-verifier0.test.ts
@@ -2,48 +2,48 @@ import verifyPassword from "./password-verifier0";
 
 test("잘못된 이름이 붙은 테스트", () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const 잘못된_룰 = (_: string) => {
+  const 실패하는_룰 = (_: string) => {
     return { passed: false, reason: "그냥 실패함" };
   };
 
-  const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+  const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
   expect(errors[0]).toMatch("그냥 실패함");
 });
 
-test("verifyPassword, 잘못된 룰을 입력할 경우 에러를 반환한다.", () => {
+test("verifyPassword, 실패하는 룰을 입력할 경우 에러를 반환한다.", () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const 잘못된_룰 = (_: string) => {
+  const 실패하는_룰 = (_: string) => {
     return { passed: false, reason: "그냥 실패함" };
   };
 
-  const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+  const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
   expect(errors[0]).toContain("그냥 실패함");
 });
 
 describe("verifyPassword", () => {
-  test("잘못된 룰을 입력할 경우 에러를 반환한다.", () => {
+  test("실패하는 룰을 입력할 경우 에러를 반환한다.", () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const 잘못된_룰 = (_: string) => {
+    const 실패하는_룰 = (_: string) => {
       return { passed: false, reason: "그냥 실패함" };
     };
 
-    const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+    const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
     expect(errors[0]).toContain("그냥 실패함");
   });
 });
 
 describe("verifyPassword", () => {
-  describe("잘못된 룰을 입력할 경우", () => {
+  describe("실패하는 룰을 입력할 경우", () => {
     test("에러를 반환한다.", () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const 잘못된_룰 = (_: string) => {
+      const 실패하는_룰 = (_: string) => {
         return { passed: false, reason: "그냥 실패함" };
       };
 
-      const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+      const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
       expect(errors[0]).toContain("그냥 실패함");
     });
@@ -51,14 +51,14 @@ describe("verifyPassword", () => {
 });
 
 describe("verifyPassword", () => {
-  describe("잘못된 룰을 입력할 경우", () => {
+  describe("실패하는 룰을 입력할 경우", () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const 잘못된_룰 = (_: string) => {
+    const 실패하는_룰 = (_: string) => {
       return { passed: false, reason: "그냥 실패함" };
     }; // 이 룰은 describe 스코프 내에서 재사용할 수 있음
 
     test("에러를 반환한다.", () => {
-      const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+      const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
       expect(errors[0]).toContain("그냥 실패함");
     });
@@ -66,14 +66,14 @@ describe("verifyPassword", () => {
 });
 
 describe("verifyPassword", () => {
-  describe("잘못된 룰을 입력할 경우", () => {
+  describe("실패하는 룰을 입력할 경우", () => {
     it("에러를 반환한다.", () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const 잘못된_룰 = (_: string) => {
+      const 실패하는_룰 = (_: string) => {
         return { passed: false, reason: "그냥 실패함" };
       };
 
-      const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+      const errors = verifyPassword("아무 값이나 넣음", [실패하는_룰]);
 
       expect(errors[0]).toContain("그냥 실패함");
     });
