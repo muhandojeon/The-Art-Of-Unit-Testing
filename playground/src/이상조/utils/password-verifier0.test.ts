d7d02bc664ee85d4ffa7549858af7442a68a4235
sjoleee <cent7425@gmail.com>
2025-02-18 21:21:30 +0900

    USE 전략

--- playground/src/이상조/utils/password-verifier0.test.ts
+++ playground/src/이상조/utils/password-verifier0.test.ts
@@ -10,3 +10,14 @@ test("잘못된 이름이 붙은 테스트", () => {
 
   expect(errors[0]).toMatch("그냥 실패함");
 });
+
+test("verifyPassword, 잘못된 룰을 입력할 경우 에러를 반환한다.", () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const 잘못된_룰 = (_: string) => {
+    return { passed: false, reason: "그냥 실패함" };
+  };
+
+  const errors = verifyPassword("아무 값이나 넣음", [잘못된_룰]);
+
+  expect(errors[0]).toContain("그냥 실패함");
+});
