9a795896b76de894b11b2b1b9cec668e2e2735fc
sjoleee <cent7425@gmail.com>
2025-02-18 21:30:12 +0900

    verifier1 작성

---
+++ playground/src/이상조/utils/password-verifier1.ts
@@ -0,0 +1,30 @@
+type Rule = (input: string) => {
+  passed: boolean;
+  reason: string;
+};
+
+class PasswordVerifier1 {
+  private rules: Rule[] = [];
+
+  constructor() {
+    this.rules = [];
+  }
+
+  addRule(rule: Rule) {
+    this.rules.push(rule);
+  }
+
+  verify(input: string) {
+    const errors: string[] = [];
+
+    this.rules.forEach((rule) => {
+      const result = rule(input);
+      if (!result.passed) {
+        errors.push(result.reason);
+      }
+    });
+    return errors;
+  }
+}
+
+export default PasswordVerifier1;
